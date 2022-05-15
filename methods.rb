aa4e4fc4cd877d0b799bdb845968c8c0475c535e
Samantha Crespo <samkcrespo@gmail.com>
2022-05-15 13:56:29 -0700

    commit tree 59bb37bf5489fe8e8621c875a2d9f6b1c79ec043

--- methods.rb
+++ methods.rb
@@ -1,1 +1,28 @@
 # Your code here!
+
+def greet_programmer
+  puts 'Hello, programmer!'
+end
+
+def greet name
+  puts "Hello, #{name}! "
+end
+
+greet 'Naureen'
+greet 'Jimmy'
+
+def greet_with_default(name='programmer')
+  puts "Hello, #{name}!"
+end
+greet_with_default('Naureen')
+greet_with_default
+
+def add num1, num2
+  num1 + num2
+end
+sum = add(1,2)
+
+def halve number
+  return nil unless number.class == Integer
+number / 2
+end
